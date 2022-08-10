dfe6448e8512bd312596296d9554f90a9b5dcb8a
Alex Minser <aminser1996@gmail.com>
2022-08-09 20:31:53 -0400

    Add device number support

--- hello_world.c
+++ hello_world.c
@@ -1,34 +1,22 @@
+//includes
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/usb.h>
+#include <linux/list.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
+//defines
 #define STATUS_SUCCESS (0)
-#define PS5_CTRL_PRODUCT_ID ()
-#define PS5_CTRL_VENDOR_ID ()
+#define PS5_CTRL_PRODUCT_ID (0x054C) //Sony ID
+#define PS5_CTRL_VENDOR_ID (0x0CE6) //PS5 controller
 
-/*
-struct usb_driver {
-  const char * name;
-  int (* probe) (struct usb_interface *intf,const struct usb_device_id *id);
-  void (* disconnect) (struct usb_interface *intf);
-  int (* ioctl) (struct usb_interface *intf, unsigned int code,void *buf);
-  int (* suspend) (struct usb_interface *intf, pm_message_t message);
-  int (* resume) (struct usb_interface *intf);
-  int (* reset_resume) (struct usb_interface *intf);
-  int (* pre_reset) (struct usb_interface *intf);
-  int (* post_reset) (struct usb_interface *intf);
-  const struct usb_device_id * id_table;
-  struct usb_dynids dynids;
-  struct usbdrv_wrap drvwrap;
-  unsigned int no_dynamic_id:1;
-  unsigned int supports_autosuspend:1;
-  unsigned int soft_unbind:1;
-}; */
 
+//declarations
 static int ps5_ctrl_probe(struct usb_interface* intf, unsigned int code, void* buf);
 
+
+//globals
 static struct usb_driver ps5_ctrl_driver = {
     .name = "PS5 controller driver",
     .probe = ps5_ctrl_probe
@@ -39,8 +27,12 @@ static struct usb_device_id ps5_ctrl_table [] = {
         { USB_DEVICE(PS5_CTRL_VENDOR_ID, PS5_CTRL_PRODUCT_ID) },
         { }                      /* Terminating entry */
 };
+MODULE_DEVICE_TABLE(usb, ps5_ctrl_table);
 
+static LIST_HEAD(controllers);
+//TODO: protect w/ mutex
 
+//procedures
 static int __init hello_init(void)
 {
     int res = 0;
@@ -50,7 +42,6 @@ static int __init hello_init(void)
         printk("usb_register() failed with code %d\n", res);
         return res;
     }
-    //and don't forget MODULE_DEVICE_TABLE(usb, ...)
     return 0;
 }
 
@@ -58,11 +49,24 @@ static void __exit hello_exit(void)
 {
     printk(KERN_ALERT "Module exit\n");
     usb_deregister(&ps5_ctrl_driver);
+    dev_t curr_dev;
+    list_for_each(&curr_dev, controllers) {
+        //TODO: feel like this isn't the right way to do this
+        unregister_chrdev_region(*curr_dev, 1);
+    }
 }
 
 static int ps5_ctrl_probe(struct usb_interface* intf, unsigned int code, void* buf) {
-    printk("probe activated!\n");
+    printk(KERN_ALERT "probe activated!\n");
+    dev_t new_dev;
+    ret = alloc_chrdev_region(&new_dev, 0, 1, "ps5_ctrl");
+    if (ret) {
+        printk(KENR_ALERT "Failed to alloc ps5 ctrl!");
+        return ret;
+    }
+    return 0;
 }
 
+//run
 module_init(hello_init);
 module_exit(hello_exit);
